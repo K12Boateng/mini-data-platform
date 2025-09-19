703b2db08c746b4f5f6af5c3e9e2b70fa2664564
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-09-19 15:00:36 +0000

    feat: Import packages for dags

---
+++ dags/process_sales_dags.py
@@ -0,0 +1,19 @@
+  - Wait for files in incoming/
+  - List files
+  - For each file:
+      - Validate format/schema
+      - If invalid -> quarantine
+      - If valid -> process -> load to Postgres -> finalize
+  - Log all events to file_ingestion_log
+  - Move files to processed/ or failed/<reason>/
+  - Send email notifications on DAG success/failure
+"""
+
+import io
+import logging
+import sys
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+from airflow.sensors.python import PythonSensor
+from airflow.utils.email import send_email
